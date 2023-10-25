abb6b681b2e3702dab74dc413ea5e4ce1160fea5
Rushikesh Wagh <74753184+iamrushi353@users.noreply.github.com>
2023-10-25 14:57:35 +0000

    Update Jumbutron.js

--- src/components/Jumbutron.js
+++ src/components/Jumbutron.js
@@ -5,7 +5,7 @@ const Jumbutron = ({ children }) => {
     <div className="bg-gray-900 flex items-center py-10">
       <div className="max-w-md mx-auto w-full">
         <h1 className="text-white text-center text-3xl font-bold mb-5">
-          Image Gallery
+          Image Gallery 
         </h1>
         {children}
       </div>
